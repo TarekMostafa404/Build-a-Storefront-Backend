9d2e329c8827b7cb1e8fd2a78e7c9beee27295a0
Tarek Mostafa <tarekmostafa.dev@gmail.com>
2022-02-05 22:56:09 +0200

    res

--- src/handlers/product-api.ts
+++ src/handlers/product-api.ts
@@ -3,13 +3,13 @@ import {ProductStore, Product} from "../models/product";
 
 const productRoutes = express.Router();
 
-productRoutes.get('/product/index', async (req:Request, res: Response)=>{
+productRoutes.get('/product/index', async (_req:Request, res: Response)=>{
     const store = new ProductStore();
         const result = await store.index()
         res.send(result)
     })
 
-   productRoutes.get('/product/create', async (req:Request, res: Response)=>{
+   productRoutes.get('/product/create', async (_req:Request, res: Response)=>{
         const store = new ProductStore();
         const p: Product = {id:1, name:'p1', price:100, category:'cat'}
             const result = await store.create(p)
