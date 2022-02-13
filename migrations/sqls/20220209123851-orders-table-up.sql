3f130168ff8dc41c03f14e4a18773a774f7e8f72
Tarek Mostafa <80650764+TarekMostafa404@users.noreply.github.com>
2022-02-13 14:53:02 +0200

    db

--- migrations/sqls/20220209123851-orders-table-up.sql
+++ migrations/sqls/20220209123851-orders-table-up.sql
@@ -2,6 +2,6 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     quantity INTEGER NOT NULL,
-    status VARCHAR(50) NOT NULL
-    user_id INTEGER REFERENCES users(id) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    user_id INTEGER REFERENCES users(id) NOT NULL
 );
